d3782613d350b160e9b76fc161a5f5fbd4daa1b8
Max Brooks <maxbrooks114@gmail.com>
2019-02-04 21:32:39 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.scan(/[[:punct:]]/).count > 4
+   self.scan(/[[:punct:]]/).uniq.count
+  else 
+   self.scan(/[[:punct:]]/).count
+ end
   end
 end
