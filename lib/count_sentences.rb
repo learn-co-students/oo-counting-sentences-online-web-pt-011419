f6b00cce979b8ce83dc5c5aaf8cc4761fcfafa8c
A Glazer <avigayilglazer@gmail.com>
2019-02-04 18:14:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,10 @@ class String
   end
 
   def count_sentences
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+#the first [^\.!?] - ^ is looking for all characters that are not #listed
+# + is checking unlimited amount of times (therefore taking out ... or !!) until we get the 2nd set of [\.!?]
+# scan has regex to split string and strip will remove trailing #spaces.
 
   end
 end
