7b10d92cb000a9a8553e9a115d9c1ed54c90c94b
Ryan Mui <reachryanmui@gmail.com>
2019-02-17 07:05:16 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    splitArray = self.split(".")
+    splitArray.each do |element|
+      binding.pry
+      element.split("?")
+    end
   end
 end
